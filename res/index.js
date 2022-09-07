cf0124d13e7a409f1ebd361868a45329e493b2ee
DerivativeMarmot <derivativetu@gmail.com>
2022-09-07 00:01:40 -0700

    typechange: index.css 	typechange: index.html 	typechange: index.js 	new file:   res/index.css 	new file:   res/index.html 	new file:   res/index.js 	renamed:    logo/default-logo.jpg -> res/logo/default-logo.jpg 	renamed:    logo/hexo-logo.png -> res/logo/hexo-logo.png 	renamed:    webapp.json -> res/webapp.json

---
+++ res/index.js
@@ -0,0 +1,51 @@
+fetch("./res/webapp.json")
+    .then(response => {
+        return response.json();
+    })
+    .then(res => {
+        let content = document.createElement("div");
+        content.setAttribute("id", "content");
+        let title = document.createElement("div");
+        title.setAttribute("id", "title");
+        title.innerHTML = "Dashboard";
+        content.appendChild(title);
+
+        let webapps = document.createElement("div");
+        webapps.setAttribute("id", "webapp");
+        for (let app in res) {
+            let app_dict = res[app];
+            let wa = document.createElement("div");
+            wa.setAttribute("id", app);
+            wa.setAttribute("class", "block");
+            
+            let logo = document.createElement("div");
+            logo.setAttribute("id", "block-logo");
+            let img = document.createElement("img");
+            if (app_dict.logo_path){
+                
+                img.setAttribute("src", app_dict.logo_path);
+            }else{
+                img.setAttribute("src", "./res/logo/default-logo.jpg");
+            }
+            logo.appendChild(img);
+            wa.appendChild(logo);
+
+            let name = document.createElement("div");
+            name.setAttribute("id", "block-name");
+            name.innerHTML = app_dict.display_name;
+            wa.appendChild(name);
+
+            if (app_dict.target) {
+                let a = document.createElement("a");
+                a.setAttribute("href", app_dict.target);
+                a.appendChild(wa);
+                webapps.appendChild(a);
+            }
+            else {
+                webapps.appendChild(wa);
+            }
+
+        }
+        content.appendChild(webapps);
+        document.body.appendChild(content);
+    });
